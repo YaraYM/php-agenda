e3fd82a3f12184f2083b36192681886b2e43162d
YaraYM <yaraym0408@gmail.com>
2024-09-30 15:37:00 -0300

    Inserindo contatos na agenda

--- config/process.php
+++ config/process.php
@@ -5,14 +5,53 @@
   include_once("connection.php");
   include_once("url.php");
 
-  $id;
+  $data = $_POST;
 
-  if(!empty($_GET)) {
-    $id = $_GET["id"];
-  }
+  // modificações no banco
+  if(!empty($data)) {
+  
+    // Criar contato
+    if($data["type"] == "create") {
 
-  // Retorna o dado de um contato
-  if(!empty($id)) {
+      $name = $data["name"];
+      $phone = $data["phone"];
+      $observations = $data["observations"];
+
+      $query = "INSERT INTO contacts (name, phone, observations) VALUES (:name, :phone, :observations)";
+
+      $stmt = $conn->prepare($query);
+
+      $stmt->bindParam(":name", $name);
+      $stmt->bindParam(":phone", $phone);
+      $stmt->bindParam(":observations", $observations);
+
+      try {
+
+        $stmt->execute();
+        $_SESSION["msg"] = "Contato criado com sucesso!";
+
+      } catch (PDOException $e) {
+        // erro na conexão
+        echo $e->getMessage();
+        echo "Erro: $error";
+      }
+    
+    }
+
+    // redirect home
+    header("Location:" . $BASE_URL . "../index.php");
+
+  // seleção de dados
+  } else {
+
+    $id;
+
+    if(!empty($_GET)) {
+      $id = $_GET["id"];
+    }
+
+    // Retorna o dado de um contato
+    if(!empty($id)) {
 
     $query = "SELECT * FROM contacts WHERE id = :id";
     $stmt = $conn->prepare($query);
@@ -20,13 +59,17 @@
     $stmt->execute();
     $contact = $stmt->fetch();
 
-  } else {
-    // Retorna todos os contatos
-    $contacts = [];
-    $query = "SELECT * FROM contacts";
-    $stmt = $conn->prepare($query);
-    $stmt->execute();
-    $contacts = $stmt->fetchAll();
+    } else {
+      // Retorna todos os contatos
+      $contacts = [];
+      $query = "SELECT * FROM contacts";
+      $stmt = $conn->prepare($query);
+      $stmt->execute();
+      $contacts = $stmt->fetchAll();
+    }
   }
+  
+  // fechar conexão
+  $conn = null;
 
 ?>
